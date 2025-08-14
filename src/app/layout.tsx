bc0a8c5f01efaacc441dd88ae430b16bfb93591e
vzwtim <yudai.grex4@gmail.com>
2025-08-15 03:22:48 +0900

    remove external fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,18 +16,6 @@ export default function RootLayout({
 }): React.JSX.Element {
   return (
     <html lang="ja">
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="anonymous"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Shippori+Mincho:wght@400;500;600;700;800&family=Noto+Sans+JP:wght@400;500;700&display=swap"
-          rel="stylesheet"
-        />
-      </head>
       <body>
         <ClientLayout>
           {children}
